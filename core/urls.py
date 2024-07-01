04ef3e67f3e21884df3a03ed69a780740086bce7
Proyecto <alo.gonzalezj@duocuc.cl>
2024-06-30 20:21:01 -0400

    Empleados Agregados

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name="index"),
+    path('empleados/', empleados, name="empleados"),
+    path('empleados/add/', empleadosadd, name="empleadosadd"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def empleados(request):
+    return render(request, 'core/empleados/index.html')
+
+def empleadosadd(request):
+    return render(request, 'core/empleados/crud/add.html')    
