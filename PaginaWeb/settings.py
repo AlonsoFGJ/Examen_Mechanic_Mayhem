242e77f8f8e0d2d512ee6aeb287ca20af0d3907d
Proyecto <alo.gonzalezj@duocuc.cl>
2024-07-05 00:00:01 -0400

    Lista Emp

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
 ]
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
 TIME_ZONE = 'UTC'
 
--- core/views.py
+++ core/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
 
 def empleados(request):
-    return render(request, 'core/empleados/index.html')
+    empleados = Empleado.objects.all()
+
+    aux = {
+        'lista' : empleados
+    }
+ 
+    return render(request, 'core/empleados/index.html', aux)
 
 def empleadosadd(request):
     return render(request, 'core/empleados/crud/add.html')    
+
+def empleadosupdate(request):
+    return render(request, 'core/empleados/crud/update.html')   
+
+def empleadosdelete(request):
+    return render(request, 'core/empleados/crud/add.html')   
